438c4a17a36ed9637d30d4d76c6c78da06cb1a52
Hasan Kashi <61319182+hasan-kashi@users.noreply.github.com>
2020-11-04 15:48:00 +0330

    Add files via upload

--- src/compiler/compiled.c
+++ src/compiler/compiled.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <conio.h>
-   int main(int argc, char *argv[]){  
-char x [100000] = "sfs" ;
-int x = 1 ;return 0; }
+   int main(int argc, char *argv[]){  printf("hello") ;return 0; }
